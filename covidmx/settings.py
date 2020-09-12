fa9c6e5cc5487225ae45a802af3c036dafe1a849
Jorge A. Pérez Garay <jperez@sw1848.com>
2020-09-11 23:14:51 -0500

    Allowed host

--- covidmx/settings.py
+++ covidmx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6oofk!p=0j1@hgq-1a-cq##o&axdozh)ga&&(8by8idr3&om@+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
